026daa0828409c837b88aad023320392e37351ae
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-13 15:03:57 +0100

    cluster.h

--- c-skeleton/headers/cluster.h
+++ c-skeleton/headers/cluster.h
@@ -3,9 +3,11 @@
 #ifndef CLUSTER_H
 #define CLUSTER_H
 
+/*
+Structure qui permet de définir les clusters et les centroïds tel que 
+cluster est défini par cluster_t** et centroids est défini par cluster_t* 
+*/ 
 typedef struct {
-    int x;
-    int y;
 } cluster_t;
 
 #endif //CLUSTER_H
