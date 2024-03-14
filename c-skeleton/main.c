6c9fdde92fb50eaf0658f0b22bcf30d7537496e1
Louan Buckinx <louan.buckinx@student.uclouvain.be>
2024-03-14 17:07:55 +0000

    Update main.c

--- c-skeleton/main.c
+++ c-skeleton/main.c
@@ -135,6 +135,18 @@ int main(int argc, char *argv[]) {
 
 
 
+#--
+    uint64_t distortion(cluster_t *centroids, cluster_t **clusters, uint32_t num_clusters) {
+        uint64_t current_sum = 0;
+        for (uint32_t k = 0; k < num_clusters; ++k) {
+            for (uint64_t j = 0; j < clusters[k]->surrounding; ++j) {
+                current_sum += DISTANCE_SQUARED;
+            }
+        }
+        return current_sum;
+    }
+#--
+
 
 
     // TODO: parse the binary input file, compute the k-means solutions and write the output in a csv
