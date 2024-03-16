5d2ba4b8015485168ef7af7fef6b96ef0c88c50d
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-16 19:44:35 +0100

    update_centroids.h

--- c-skeleton/headers/update_centroids.h
+++ c-skeleton/headers/update_centroids.h
@@ -10,5 +10,4 @@
 Cette fonction calcule le centroïd associé à un cluster donné.
 La fonction return -1 si une erreur se produit, 0 sinon.
 */
-cluster_t update_centroids(cluster_t* clusters){
-}
+uint64_t update_centroids(cluster_t* clusters);
