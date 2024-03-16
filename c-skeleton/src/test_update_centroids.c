a6241baec5b0495f0a1d5a7c3b1bae52debaa24b
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-16 19:45:10 +0100

    update

--- c-skeleton/src/test_update_centroids.c
+++ c-skeleton/src/test_update_centroids.c
@@ -1,52 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
-//#include "unity.h"
+#include "../headers/cluster.h" 
+#include "../headers/point.h" 
+#include "../headers/update_centroids.h" 
+// Définition des constantes K et dimension
+#define K 10
 
-#include <CUnit/Basic.h>
-#include "../headers/update_centroids.h"
 
-// Définition de la constante K
-#define K 3
 
-// Déclaration de la fonction à tester
-int64_t update_centroids(cluster_t *clusters);
-
-// Fonction de configuration des tests
-void setUp(void) {}
-
-// Fonction de nettoyage des tests
-void tearDown(void) {}
-
-// Test de la fonction update_centroids
-void test_update_centroids(void) {
-    // Création des données de test
-    cluster_t clusters[K];
+// Fonction pour afficher les données d'un cluster
+void print_cluster(cluster_t cluster) {
+    printf("Cluster:\n");
     for (int i = 0; i < K; i++) {
-        clusters[i].data = (point_t *)malloc(sizeof(point_t));
-        clusters[i].data->dim = 2;
-        clusters[i].data->coord = (int64_t *)malloc(2 * sizeof(int64_t));
-        clusters[i].data->coord[0] = i;
-        clusters[i].data->coord[1] = i + 1;
-    }
-
-    // Appel de la fonction à tester
-    int64_t result = update_centroids(clusters);
-
-    // Vérification du résultat
-    TEST_ASSERT_EQUAL_INT64(0, result);
-
-    // Libération de la mémoire allouée
-    for (int i = 0; i < K; i++) {
-        free(clusters[i].data->coord);
-        free(clusters[i].data);
+        printf("\tData %d: ", i);
+        for (uint32_t j = 0; j < cluster.data[i].dim; j++) {
+            printf("%ld ", cluster.data[i].coords[j]);
+        }
+        printf("\n");
     }
 }
 
-int main(void) {
-    
-    UNITY_BEGIN();
-    RUN_TEST(test_update_centroids);
-    return UNITY_END();
-} 
+int main() {
+    // Création de clusters de données de test
+    cluster_t clusters[K];
+    for (int i = 0; i < K; i++) {
+        clusters[i].data = (point_t *)malloc(sizeof(point_t));
+        clusters[i].data[0].dim = 3;
+        clusters[i].data[0].coords = (int64_t *)malloc(3 * sizeof(int64_t));
+        for (int j = 0; j < 3; j++) {
+            clusters[i].data[0].coords[j] = (i + 1) * (j + 1);
+        }
+    }
+
+    // Appel de la fonction update_centroids
+    cluster_t centroids = update_centroids(clusters);
+
+    // Affichage des centroids
+    print_cluster(centroids);
+
+    // Libération de la mémoire allouée
+    for (int i = 0; i < K; i++) {
+        free(clusters[i].data[0].coords);
+        free(clusters[i].data);
+    }
+    free(centroids.data);
+
+    return 0;
+}
