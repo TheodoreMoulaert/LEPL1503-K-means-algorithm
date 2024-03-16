de101b5c1fa9e488b66c3fda036a63442f8363ef
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-16 12:45:03 +0100

    update_centroids.c

--- c-skeleton/src/update_centroids.c
+++ c-skeleton/src/update_centroids.c
@@ -3,21 +3,15 @@
 #include <stdint.h>
 #include <string.h>
 
-typedef struct{
-    int64_t* coord; // (1,1)
-} point_t;
 
-typedef struct {
-    point_t* tuples; // [(1,1),(3,0),(3,1)]
-} cluster_t;
 
 int64_t update_centroids( cluster_t* clusters){
     cluster_t centroids;
-    centroids.tuples = (point_t*)malloc(K*sizeof(point_t));
+    centroids->data = (point_t*)malloc(K*sizeof(point_t));
     
-    if (centroids->tuples == NULL){
+    if (centroids->data == NULL){
         for (uint32_t i =0;i<K;i++){
-            free(clusters[i]->tuples->coord);
+            free(clusters[i]->data->coord);
         }
         return -1;
     }
@@ -29,14 +23,14 @@ int64_t update_centroids( cluster_t* clusters){
         if (vector_sum == NULL){
             free(vector_sum);
             for (uint32_t i =0;i<k;i++){
-                free(clusters[i]->tuples->coord);
+                free(clusters[i]->data->coord);
             }
             return -1;
         }
 
         for (uint32_t i=0; i <clusters_length;i++){
             for (uint32_t m =0;m<dimension;m++){
-                vector_sum[m] += clusters[k].tuples[i].coord[m];
+                vector_sum[m] += clusters[k].data[i].coord[m];
             }
         }
 
@@ -44,12 +38,12 @@ int64_t update_centroids( cluster_t* clusters){
             vector_sum[m] = vector_sum[m]/clusters_length;
         }
 
-        centroids.tuples[k] = vector_sum;
+        centroids.data[k] = vector_sum;
         //free(vector_sum);
     }
 
     for (int i =0; i<K;i++){
-        free(centroids.tuples[i]);
+        free(centroids.data[i]);
     }
     free(centroids);
     
