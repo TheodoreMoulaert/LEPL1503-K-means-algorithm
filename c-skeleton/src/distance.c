5c83069132b7ddc0405d502b38cce0136f67e5ef
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-03-16 12:57:21 +0100

    mise à jour distance en fonction de la définition de point

--- c-skeleton/src/distance.c
+++ c-skeleton/src/distance.c
@@ -19,22 +19,14 @@ int64_t squared_manhattan_distance(const point_t *p1, const point_t *p2) {
     result : la distance de Manhattan au carré entre les deux points
     */
     
+    
     int64_t result; 
-    if(dimension == 2 ){
-        int64_t dx = p2->x - p1->x; 
-        int64_t dy = p2->y - p1->y; 
-        int64_t val = llabs(dx)+llabs(dy);
-        result = val*val; 
-    }else if( dimension == 3){
-        int64_t dx = p2->x - p1->x; 
-        int64_t dy = p2->y - p1->y; 
-        int64_t dz = p2->z - p1->z; 
-        int64_t val = llabs(dx)+llabs(dy) + llabs(dz);
-        result = val*val; 
-
+    uint32_t dim = p1->dim; 
+    for(uint32_t i; i< dim; i++){
+        result = result+ llabs(p2->coord[i] - p1->coord[i] ); 
+        
     }
-    // TODO
-    return result;
+    return result*result;
 }
 
 
@@ -52,25 +44,12 @@ int64_t squared_euclidean_distance(const point_t *p1, const point_t *p2) {
     */
 
     int64_t result; 
-    if(dimension == 2){
-        int64_t dx = p1->x - p2->x; 
-        int64_t dy = p1->y - p2->y; 
-        int64_t carre_x = dx*dx; 
-        int64_t carre_y = dy*dy; 
-        result = carre_x + carre_y;
+    uint32_t dim = p1->dim; 
 
+    for (uint32_t i = 0; i< dim; i++){
+        int64_t diff = p1->coord[i]- p2->coord[i]; 
+        result += diff*diff;
     }
-    else if(dimension == 3){
-        int64_t dx = p1->x - p2->x; 
-        int64_t dy = p1->y - p2->y; 
-        int64_t dz = p1->z - p2->z; 
-        int64_t carre_x = dx*dx; 
-        int64_t carre_y = dy*dy; 
-        int64_t carre_z = dz*dz; 
-        result = carre_x + carre_y+ carre_z;
-
-    }
-    // TODO
     return result;
 }
 
