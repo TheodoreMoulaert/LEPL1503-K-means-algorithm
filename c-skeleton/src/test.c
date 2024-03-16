199696ff2ff7411319ec66e89b46b9c28e2fb584
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-03-16 16:24:42 +0100

    ajout de test

--- c-skeleton/src/test.c
+++ c-skeleton/src/test.c
@@ -11,8 +11,14 @@
 #include "../headers/binary_file_reader.h" // Inclure le bon en-tête
 #include "../headers/distance.h" // Inclure si nécessaire
 
+
+
 // Déclaration de la fonction test_point_input
 void test_get_dimension_from_binary_file();
+void test_get_nbr_vectors_from_binary_file(); 
+
+
+
 
 // Définition de la fonction de test
 void test_get_dimension_from_binary_file() {
@@ -28,6 +34,22 @@ void test_get_dimension_from_binary_file() {
     printf("Dimension lue depuis le fichier binaire : %u\n", dim);
 }
 
+void test_get_nbr_vectors_from_binary_file() {
+    // Ouverture du fichier binaire
+    FILE *file = fopen("../python/exemple.bin", "rb");
+    if (!file) {
+        perror("Erreur lors de l'ouverture du fichier binaire");
+        exit(EXIT_FAILURE);
+    }
+
+    // Lecture du nombre de vecteurs à partir du fichier
+    uint64_t nbr_vectors = get_nbr_vectors_from_binary_file(file);
+    fclose(file);
+
+    // Affichage du nombre de vecteurs
+    printf("Nombre de vecteurs dans le fichier binaire : %lu\n", nbr_vectors);
+}
+
 // Fonction principale pour exécuter les tests
 int main() {
     CU_initialize_registry(); // Initialisation du registre de tests
@@ -37,6 +59,7 @@ int main() {
 
     // Ajout des tests à la suite
     CU_add_test(suite, "Test_dim", test_get_dimension_from_binary_file);
+    CU_add_test(suite, "Test_nbr_vectors", test_get_nbr_vectors_from_binary_file);
 
     // Exécution des tests
     CU_basic_run_tests();
