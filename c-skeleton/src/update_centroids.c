a7f432d2abfec7fa92311f2ed392b69b22739747
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-16 12:39:34 +0100

    update_centroids.c

--- c-skeleton/src/update_centroids.c
+++ c-skeleton/src/update_centroids.c
@@ -8,14 +8,14 @@ typedef struct{
 } point_t;
 
 typedef struct {
-    point_t tuples; // (1,1)
+    point_t* tuples; // [(1,1),(3,0),(3,1)]
 } cluster_t;
 
 int64_t update_centroids( cluster_t* clusters){
     cluster_t centroids;
     centroids.tuples = (point_t*)malloc(K*sizeof(point_t));
     
-    if (centroids.tuples == NULL){
+    if (centroids->tuples == NULL){
         for (uint32_t i =0;i<K;i++){
             free(clusters[i]->tuples->coord);
         }
@@ -24,35 +24,35 @@ int64_t update_centroids( cluster_t* clusters){
 
     for (int k=0;k < K;k++){
         int64_t clusters_length = (int64_t)strlen(clusters[k]);
-        point_t vector_sum;
-        vector_sum->coord = (int64_t *)calloc(dimension,sizeof(int64_t)); //un tuple
+        int64_t* vector_sum = (int64_t *)calloc(dimension,sizeof(int64_t)); //un tuple
         
-        if (vector_sum->coord == NULL){
-            free(vector_sum->coord);
+        if (vector_sum == NULL){
+            free(vector_sum);
             for (uint32_t i =0;i<k;i++){
-                free(clusters[i].tuples->coord);
+                free(clusters[i]->tuples->coord);
             }
             return -1;
         }
 
         for (uint32_t i=0; i <clusters_length;i++){
             for (uint32_t m =0;m<dimension;m++){
-                vector_sum.coord[m] += clusters[k].tuples[i].coord[m];
+                vector_sum[m] += clusters[k].tuples[i].coord[m];
             }
         }
 
         for (int m=0;m<dimension;m++){
-            vector_sum.coord[m] = vector_sum.coord[m]/clusters_length;
+            vector_sum[m] = vector_sum[m]/clusters_length;
         }
 
         centroids.tuples[k] = vector_sum;
+        //free(vector_sum);
     }
 
     for (int i =0; i<K;i++){
         free(centroids.tuples[i]);
     }
-    //free(centroids);
-    free(vector_sum);
+    free(centroids);
+    
     return 0;
 
 }
