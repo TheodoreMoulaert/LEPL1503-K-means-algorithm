cd66cdd5e12a92c2856ce438aec11549c89a03ba
Victor Defrenne <victor.defrenne@student.uclouvain.be>
2024-03-18 12:36:49 +0000

    Update assign vector to centro.c

--- c-skeleton/src/assign vector to centro.c
+++ c-skeleton/src/assign vector to centro.c
@@ -6,8 +6,36 @@ int assign_vectors_to_centroids(double **centroids, double ***clusters, int num_
         fprintf(stderr, "\tAssign points to centroids\n");
     }
 
+    
+    double ***new_clusters = (double ***)malloc(num_centroids * sizeof(double **));
+    if (new_clusters == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+
+    for (i = 0; i < num_centroids; i++) {
+        new_clusters[i] = (double **)malloc(/* Size of cluster */ * sizeof(double *));
+        if (new_clusters[i] == NULL) {
+            fprintf(stderr, "Memory allocation failed\n");
+            return 1;
+        }
+    }
+
+
     for (i = 0; i < num_centroids; i++) {
         for (j = 0; j < /* Size of cluster */; j++) {
+            new_clusters[i][j] = (double *)malloc(dimensions * sizeof(double));
+            if (new_clusters[i][j] == NULL) {
+                fprintf(stderr, "Memory allocation failed\n");
+                return 1;
+            }
+        }
+    }
+
+
+    for (i = 0; i < num_centroids; i++) {
+        for (j = 0; j < /* Size of cluster */; j++) {
+
             int closest_centroid_idx;
             double closest_centroid_distance = INFINITY;
 
@@ -20,18 +48,31 @@ int assign_vectors_to_centroids(double **centroids, double ***clusters, int num_
                 }
             }
 
+
             if (LOG) {
                 fprintf(stderr, "\t\t%s closest to %s (before %s)\n", clusters[i][j], centroids[closest_centroid_idx], centroids[i]);
             }
-            
+
             memcpy(new_clusters[closest_centroid_idx][j], clusters[i][j], dimensions * sizeof(double));
 
-            
+
             unchanged = unchanged && closest_centroid_idx == i;
         }
     }
-}
 
-    
+
+    for (i = 0; i < num_centroids; i++) {
+        for (j = 0; j < /* Size of cluster */; j++) {
+            free(clusters[i][j]);
+        }
+        free(clusters[i]);
+    }
+    free(clusters);
+
+
+    clusters = new_clusters;
+
+    return !unchanged;
+}
 
     
