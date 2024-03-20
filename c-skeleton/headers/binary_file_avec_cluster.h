e5e2c8cf1627aeb56e200618462e44288dab5f5d
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-03-20 17:35:10 +0100

    nouveau

---
+++ c-skeleton/headers/binary_file_avec_cluster.h
@@ -0,0 +1,31 @@
+#ifndef BINARY_FILE_READER_H
+#define BINARY_FILE_READER_H
+#include "../headers/point.h" // Inclure si nécessaire
+
+
+#include <stdint.h>
+#include <stdio.h> // Inclure pour FILE *
+
+// Déclaration de la fonction pour obtenir la dimension des points et le nombre de vecteurs à partir du fichier binaire
+
+
+uint32_t get_dimension_from_binary_file(FILE* file);
+
+
+// Déclaration de la fonction pour obtenir le nombre de vecteurs à partir du fichier binaire
+
+uint64_t get_nbr_vectors_from_binary_file(FILE *file);
+
+// Déclaration de la fonction pour obtenir les informations sur les coordonnées des points
+
+point_t** point_input(FILE* file);
+
+//Déclaration de la fonction pour obtenir le nombre de clusters 
+
+uint64_t size_clusters( point_t** clusters);
+
+// Déclaration de la fonction pour free
+
+void free_vectors(point_t **vectors, uint64_t nbr_vectors);
+
+#endif // BINARY_FILE_READER_H
