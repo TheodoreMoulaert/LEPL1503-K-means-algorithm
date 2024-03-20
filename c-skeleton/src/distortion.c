a799e1b2bfb3a4508a1df8c65d42868abc810d7c
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-03-20 18:33:57 +0100

    update

--- c-skeleton/src/distortion.c
+++ c-skeleton/src/distortion.c
@@ -5,17 +5,17 @@
 #include "../headers/main.h"
 #include "../headers/cluster.h"
 
-uint64_t distortion(cluster_t const **clusters, uint32_t k, squared_distance_func_t func) {
+uint64_t distortion(cluster_t const **clusters, uint32_t num_clusters, squared_distance_func_t DISTANCE_SQUARED) {
     uint64_t sum_totale = 0;
     
-    for(uint32_t i = 0; i < k; i++) {
-        uint64_t sum = 0;
+    for(uint32_t i = 0; i < num_clusters; i++) {
+        uint64_t current_sum = 0;
 
         for (uint64_t j = 0; j < clusters[i]->size; j++) {
-            sum += func(&clusters[i]->center, &clusters[i]->data[j]);
+            current_sum += DISTANCE_SQUARED(&clusters[i]->center, &clusters[i]->data[j]);
         }
 
-        sum_totale += sum;
+        sum_totale += current_sum;
     }
     
     return sum_totale; 
