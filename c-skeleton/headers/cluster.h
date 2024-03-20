0e4013c5ef8cf018cf31f5bd1eb8d6e642ade0e8
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-20 11:56:09 +0100

    test_up_centro_f2

--- c-skeleton/headers/cluster.h
+++ c-skeleton/headers/cluster.h
@@ -7,9 +7,9 @@ Structure qui  permet de définir les clusters et les centroïds tel que
 cluster est défini par cluster_t** et centroids est défini par cluster_t* 
 */ 
 typedef struct {
-    point_t center;
-    point_t *data;
-    uint64_t size;
+    //point_t center;
+    point_t *data; // [[0,0],[0,0],[1,1]] == liste de vecteurs
+    uint64_t size; // nbr de vecteur 
 } cluster_t;
 
 #endif //CLUSTER_H
--- c-skeleton/tests_file/test_up_centro_f2.c
+++ c-skeleton/tests_file/test_up_centro_f2.c
@@ -31,51 +31,56 @@ void test_up_centro_f2(){
         fprintf(stderr, "La fonction point_input a renvoyé NULL\n");
         return;
     }
-    uint64_t vector_count = vectors[0]->nbr_vector;
+    uint64_t size = vectors[0]->nbr_vector;
     uint32_t dim = vectors[0]->dim;
 
-    int k = 2;
-    cluster_t *clusters[2];
+    uint32_t k = 1;
+    cluster_t clusters[1];//*clusters[2];
 
-    for (uint32_t i =0; i<2;i++){
-        clusters[i] = (cluster_t *)malloc(sizeof(cluster_t));
-        clusters[i]->size =0;
+    for (uint32_t i =0; i<1;i++){
+        clusters[i] = (cluster_t)malloc(sizeof(cluster_t));
+        clusters[i]->size = 2;
         clusters[i]->data = (point_t *) malloc(sizeof(point_t));
         clusters[i]->data->dim = dim;
         clusters[i]->data->coords = (int64_t *) malloc(sizeof(int64_t) * dim);
         //clusters[i]->vectors = (point_t **) malloc(0);
 
     }
-
+    //Création du clusters
+    clusters->data[0]->coords[0] = (int64_t)1;
+    clusters->data[0]->coords[1] = (int64_t)1;
+    clusters->data[1]->coords[0] = (int64_t)2;
+    clusters->data[1]->coords[1] = (int64_t)2;
     //Initialisation des centroids
-    clusters[0]->data->coords[0] = (int64_t)1;
+    /*clusters[0]->data->coords[0] = (int64_t)1;
     clusters[0]->data->coords[1] = (int64_t)1;
     clusters[1]->data->coords[0] = (int64_t)2;
-    clusters[1]->data->coords[1] = (int64_t)2;
-
+    clusters[1]->data->coords[1] = (int64_t)2;*/
     uint64_t result;
-    for (uint32_t i =0; i<2;i++){
+    /*for (uint32_t i =0; i<2;i++){
         result = update_centroids(clusters[i]);
         CU_ASSERT_EQUAL_FATAL(result, 0);
-    }
-    
+    }*/
+    result = update_centroids(clusters);
+    CU_ASSERT_EQUAL_FATAL(result, 0);
+    fprintf(stderr, "La fonction update_centroids n'a pas d'erreurs !\n");
 
     //tests les coordonnées des centroids
-    CU_ASSERT_EQUAL(clusters[0]->data->coords[0], (int64_t )1);
-    CU_ASSERT_EQUAL(clusters[0]->data->coords[1], (int64_t )1);
-    CU_ASSERT_EQUAL(clusters[1]->data->coords[0], (int64_t )4);
-    CU_ASSERT_EQUAL(clusters[1]->data->coords[1], (int64_t )5);
+    CU_ASSERT_EQUAL(clusters->data[0]->coords[0], (int64_t )1);
+    CU_ASSERT_EQUAL(clusters->data[0]->coords[1], (int64_t )1);
+    fprintf(stderr, "Le premier centroids a les coordonnées : (1,1) \n");
+
+    //CU_ASSERT_EQUAL(clusters->data[1]->coords[0], (int64_t )4);
+    //CU_ASSERT_EQUAL(clusters->data[1]->coords[1], (int64_t )5);
 
     //verification de la taille des centroids
-    CU_ASSERT_EQUAL(clusters[0]->size,2);
-    CU_ASSERT_EQUAL(clusters[1]->size,5);
+    CU_ASSERT_EQUAL(clusters->size,2);
+    //CU_ASSERT_EQUAL(clusters[1]->size,5);
 
-    free(clusters[0]->data->coords);
-    free(clusters[0]->data);
-
-    free(clusters[1]->data->coords);
-    free(clusters[1]->data);
-
+    free(clusters->data[0]->coords);
+    free(clusters->data[0]);
+    free(clusters->data[1]->coords);
+    free(clusters->data[1]);
     fclose(file);
 
 }
