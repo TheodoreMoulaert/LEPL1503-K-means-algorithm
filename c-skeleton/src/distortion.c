04a452a9fed920fe21923d56bfa2f30bd33c49e6
Louan Buckinx <louan.buckinx@student.uclouvain.be>
2024-03-20 10:49:08 +0000

    Update distortion.c

--- c-skeleton/src/distortion.c
+++ c-skeleton/src/distortion.c
@@ -2,7 +2,7 @@ uint64_t distortion(cluster_t *centroids, cluster_t **clusters, uint32_t num_clu
         uint64_t current_sum = 0;
         for (uint32_t k = 0; k < num_clusters; ++k) {
             for (uint64_t j = 0; j < clusters[k]->size; ++j) {
-                current_sum += DISTANCE_SQUARED;
+                current_sum += distance_squared(clusters[k]->data[j], centroids[k].center);
             }
         }
         return current_sum;
