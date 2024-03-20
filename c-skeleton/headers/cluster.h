21b49c21998e8a0849b23530a1704d4c22dd57fe
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-20 16:12:59 +0100

    rajout de center

--- c-skeleton/headers/cluster.h
+++ c-skeleton/headers/cluster.h
@@ -7,7 +7,7 @@ Structure qui  permet de définir les clusters et les centroïds tel que
 cluster est défini par cluster_t** et centroids est défini par cluster_t* 
 */ 
 typedef struct {
-    //point_t center;
+    point_t center;
     point_t *data; // [[0,0],[0,0],[1,1]] == liste de vecteurs
     uint64_t size; // nbr de vecteur 
 } cluster_t;
