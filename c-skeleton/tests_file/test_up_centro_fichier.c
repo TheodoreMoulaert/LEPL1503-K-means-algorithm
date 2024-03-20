037ff279421490870f604af11aca1a21d0e24c1c
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-20 22:55:29 +0100

    cha

--- c-skeleton/tests_file/test_up_centro_fichier.c
+++ c-skeleton/tests_file/test_up_centro_fichier.c
@@ -75,7 +75,7 @@ void test_up_centro_f1() {
     clusters[1].data[0].coords[0] = (int64_t)2;
     clusters[1].data[0].coords[1] = (int64_t)2;
 
-    /*clusters[0].data[2].coords[0] = (int64_t)3;
+    clusters[0].data[2].coords[0] = (int64_t)3;
     clusters[0].data[2].coords[1] = (int64_t)4;
 
     clusters[1].data[3].coords[0] = (int64_t)5;
@@ -88,7 +88,7 @@ void test_up_centro_f1() {
     clusters[1].data[5].coords[1] = (int64_t)5;
 
     clusters[1].data[6].coords[0] = (int64_t)4;
-    clusters[1].data[6].coords[0] = (int64_t)5;*/
+    clusters[1].data[6].coords[0] = (int64_t)5;
 
     uint64_t result;
     result = update_centroids(clusters, K);
@@ -98,7 +98,7 @@ void test_up_centro_f1() {
     // Tests sur les coordonnées des centroids
     CU_ASSERT_EQUAL(clusters[0].data[0].coords[0], (int64_t)1);
     CU_ASSERT_EQUAL(clusters[0].data[0].coords[1], (int64_t)1);
-    fprintf(stderr, "Le premier centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n", clusters[0].data[1].coords[0], clusters[0].data[1].coords[1]);
+    fprintf(stderr, "Le premier centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n", clusters[0].data[0].coords[0], clusters[0].data[0].coords[1]);
     CU_ASSERT_EQUAL(clusters[1].data[0].coords[0], (int64_t)4);
     CU_ASSERT_EQUAL(clusters[1].data[0].coords[1], (int64_t)5);
     fprintf(stderr, "Le deuxième centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n", clusters[1].data[0].coords[0], clusters[1].data[0].coords[1]);
