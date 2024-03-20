c2397179bcd792be14702bd8d980930f7673f3e7
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-20 15:36:16 +0100

    tests de tout l'exemple

--- c-skeleton/tests_file/test_up_centro_f2.c
+++ c-skeleton/tests_file/test_up_centro_f2.c
@@ -31,14 +31,14 @@ void test_up_centro_f2(){
         fprintf(stderr, "La fonction point_input a renvoyé NULL\n");
         return;
     }
-    uint64_t size_clusters = vectors[0]->nbr_vector;
+    uint64_t size_clu = size_clusters(vectors); //1
     uint32_t dim = vectors[0]->dim; //2
+    uint64_t vector_count = vectors[0]->nbr_vector; //7
 
+    uint32_t K = (uint64_t)size_clu; //nombre de centroids à trouver
 
-    uint32_t K = 1; //nombre de centroids à trouver
-
-    cluster_t clusters[1];//[size_clusters];
-    for (int i = 0; i < 2; i++) {
+    cluster_t clusters[vector_count];//[size_clusters];
+    for (int i = 0; i < size_clu; i++) {
         clusters[i].size = 1; // Taille de chaque cluster
         clusters[i].data = (point_t*)malloc(dim * sizeof(point_t));
         if (clusters[i].data == NULL) {
@@ -46,7 +46,7 @@ void test_up_centro_f2(){
             CU_FAIL("Erreur d'allocation de mémoire pour les clusters");
             return;
         }
-        for (int j = 0; j < size_clusters; j++) {
+        for (int j = 0; j < vector_count; j++){
             clusters[i].data[j].dim =dim;
             clusters[i].data[j].coords = (int64_t*)malloc(dim* sizeof(int64_t));
             if (clusters[i].data[j].coords == NULL) {
@@ -64,8 +64,26 @@ void test_up_centro_f2(){
             }*/
             clusters[0].data[0].coords[0] = (int64_t)1;
             clusters[0].data[0].coords[1] = (int64_t)1;
+
             clusters[0].data[1].coords[0] = (int64_t)2;
             clusters[0].data[1].coords[1] = (int64_t)2;
+
+            clusters[0].data[2].coords[0] = (int64_t)3;
+            clusters[0].data[2].coords[1] = (int64_t)4;
+
+            clusters[0].data[3].coords[0] = (int64_t)5;
+            clusters[0].data[3].coords[1] = (int64_t)7;
+
+            clusters[0].data[4].coords[0] = (int64_t)3;
+            clusters[0].data[4].coords[1] = (int64_t)5;
+
+            clusters[0].data[5].coords[0] = (int64_t)5;
+            clusters[0].data[5].coords[1] = (int64_t)5;
+
+            clusters[0].data[6].coords[0] = (int64_t)4;
+            clusters[0].data[6].coords[0] = (int64_t)5;
+
+    
         }
     }
 
@@ -118,9 +136,9 @@ void test_up_centro_f2(){
     fprintf(stderr, "La fonction update_centroids n'a pas d'erreurs !\n");
 
     //tests les coordonnées des centroids
-    CU_ASSERT_EQUAL(clusters->data->coords[0], (int64_t)1);
-    CU_ASSERT_EQUAL(clusters->data->coords[1], (int64_t)1);
-    fprintf(stderr, "Le premier centroids a les coordonnées : (1,1) \n");
+    CU_ASSERT_EQUAL(clusters->data->coords[0], (int64_t)11);
+    CU_ASSERT_EQUAL(clusters->data->coords[1], (int64_t)14);
+    fprintf(stderr, "Le centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n",clusters->data->coords[0],clusters->data->coords[0] );
 
     //CU_ASSERT_EQUAL(clusters->data[1]->coords[0], (int64_t )4);
     //CU_ASSERT_EQUAL(clusters->data[1]->coords[1], (int64_t )5);
