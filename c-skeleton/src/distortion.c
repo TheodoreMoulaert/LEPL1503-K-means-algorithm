98dbe7bb59361745fc2a3230ecbabd229b154a35
Louan Buckinx <louan.buckinx@student.uclouvain.be>
2024-03-20 10:57:29 +0000

    Update distortion.c

--- c-skeleton/src/distortion.c
+++ c-skeleton/src/distortion.c
@@ -1,9 +1,11 @@
-<<<<<<< HEAD
+#include <stdlib.h> // Pour llabs()
+
+#include "../headers/distance.h"
+#include "../headers/point.h" // Inclure si nécessaire
+#include "../headers/main.h"
+
+
 uint64_t distortion(cluster_t *centroids, cluster_t **clusters, uint32_t num_clusters, squared_distance_func_t DISTANCE_SQUARED) {
-=======
-<<<<<<< HEAD
-uint64_t distortion(cluster_t *centroids, cluster_t **clusters, uint32_t num_clusters) {
->>>>>>> d1d4e2d (test)
         uint64_t current_sum = 0;
         for (uint32_t k = 0; k < num_clusters; ++k) {
             for (uint64_t j = 0; j < clusters[k]->size; ++j) {
@@ -13,15 +15,3 @@ uint64_t distortion(cluster_t *centroids, cluster_t **clusters, uint32_t num_clu
         return current_sum;
 }
 
-=======
-uint64_t distortion(cluster_t *centroids, cluster_t **clusters, uint32_t num_clusters) {
-        uint64_t current_sum = 0;
-        for (uint32_t k = 0; k < num_clusters; ++k) {
-            for (uint64_t j = 0; j < clusters[k]->size; ++j) {
-                current_sum += DISTANCE_SQUARED;
-            }
-        }
-        return current_sum;
-}
-
->>>>>>> 6adfe45 (test)
