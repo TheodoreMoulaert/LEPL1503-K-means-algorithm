66e1fe6f89fce70fa78eaa49d1f23989d50aa5b6
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-20 18:14:59 +0100

    nouvelles valeurs

--- c-skeleton/tests_file/test_up_centro_fichier.c
+++ c-skeleton/tests_file/test_up_centro_fichier.c
@@ -11,7 +11,7 @@
 #include "../headers/point.h"
 #include "../headers/cluster.h"
 #include "../headers/distance.h" 
-#include "../headers/binary_file_reader.h" 
+#include "../headers/binary_file_avec_cluster.h" 
 #include "../headers/update_centroids.h"
 
 void test_up_centro_f1();
@@ -36,9 +36,9 @@ void test_up_centro_f1() {
     uint64_t vector_count = vectors[0]->nbr_vector; //7
 
     //uint32_t K = (uint64_t)size_clu; //nombre de centroids à trouver
-
+    uint32_t K = 2;
     cluster_t clusters[vector_count];//[size_clusters];
-    for (int i = 0; i < 1; i++){
+    for (int i = 0; i < 2; i++){
         clusters[i].size = 1; // Taille de chaque cluster
         clusters[i].data = (point_t*)malloc(dim * sizeof(point_t));
 
@@ -74,17 +74,17 @@ void test_up_centro_f1() {
             clusters[0].data[2].coords[0] = (int64_t)3;
             clusters[0].data[2].coords[1] = (int64_t)4;
 
-            clusters[0].data[3].coords[0] = (int64_t)5;
-            clusters[0].data[3].coords[1] = (int64_t)7;
+            clusters[1].data[3].coords[0] = (int64_t)5;
+            clusters[1].data[3].coords[1] = (int64_t)7;
 
-            clusters[0].data[4].coords[0] = (int64_t)3;
-            clusters[0].data[4].coords[1] = (int64_t)5;
+            clusters[1].data[4].coords[0] = (int64_t)3;
+            clusters[1].data[4].coords[1] = (int64_t)5;
 
-            clusters[0].data[5].coords[0] = (int64_t)5;
-            clusters[0].data[5].coords[1] = (int64_t)5;
+            clusters[1].data[5].coords[0] = (int64_t)5;
+            clusters[1].data[5].coords[1] = (int64_t)5;
 
-            clusters[0].data[6].coords[0] = (int64_t)4;
-            clusters[0].data[6].coords[0] = (int64_t)5;
+            clusters[1].data[6].coords[0] = (int64_t)4;
+            clusters[1].data[6].coords[0] = (int64_t)5;
         }
     }
 
@@ -94,15 +94,22 @@ void test_up_centro_f1() {
     fprintf(stderr, "La fonction update_centroids n'a pas d'erreurs !\n");
 
     // Tests sur les coordonnées des centroids
-    CU_ASSERT_EQUAL(clusters->data->coords[0], (int64_t)11);
-    CU_ASSERT_EQUAL(clusters->data->coords[1], (int64_t)14);
-    fprintf(stderr, "Le centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n", clusters->data->coords[0], clusters->data->coords[0]);
-
+    CU_ASSERT_EQUAL(clusters[0]->data->coords[0], (int64_t)2);
+    CU_ASSERT_EQUAL(clusters[0]->data->coords[1], (int64_t)2);
+    fprintf(stderr, "Le premier centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n", clusters->data->coords[0], clusters->data->coords[0]);
+    CU_ASSERT_EQUAL(clusters[1]->data->coords[0], (int64_t)4);
+    CU_ASSERT_EQUAL(clusters[1]->data->coords[1], (int64_t)5);
+    fprintf(stderr, "Le deuxième centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n", clusters->data->coords[0], clusters->data->coords[0]);
     // Vérification de la taille des centroids
-    CU_ASSERT_EQUAL(clusters->size, 1);
+    /*CU_ASSERT_EQUAL(clusters->size, 1);*/
 
-    free(clusters->data->coords);
-    free(clusters->data);
+    free(clusters[0]->data[0]->coords);
+    free(clusters[0]->data[1]->coords);
+    free(clusters[0]->data);
+    free(clusters[1]->data[0]->coords);
+    free(clusters[1]->data[1]->coords);
+    free(clusters[1]->data);
+    free(clusters);
     fclose(file);
 }
 
