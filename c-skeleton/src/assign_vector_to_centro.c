de3b68105fd9e7d802d158fe3f1afee64245df9e
Victor Defrenne <victor.defrenne@student.uclouvain.be>
2024-03-21 00:35:27 +0000

    Update assign_vector_to_centro.c je peux pas faire de test mais j'ai corriger le bugs qu'il y avait elle devrait théoriquement fonctionner

--- c-skeleton/src/assign_vector_to_centro.c
+++ c-skeleton/src/assign_vector_to_centro.c
@@ -1,78 +1,26 @@
-int assign_vectors_to_centroids(double **centroids, double ***clusters, int num_centroids, int num_vectors, int dimensions) {
-    int i, j;
-    int unchanged = 1;
-
-    if (LOG) {
-        fprintf(stderr, "\tAssign points to centroids\n");
-    }
-
-    
-    double ***new_clusters = (double ***)malloc(num_centroids * sizeof(double **));
-    if (new_clusters == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-        return 1;
-    }
-
-    for (i = 0; i < num_centroids; i++) {
-        new_clusters[i] = (double **)malloc(/* Size of cluster */ * sizeof(double *));
-        if (new_clusters[i] == NULL) {
-            fprintf(stderr, "Memory allocation failed\n");
-            return 1;
-        }
-    }
-
-
-    for (i = 0; i < num_centroids; i++) {
-        for (j = 0; j < /* Size of cluster */; j++) {
-            new_clusters[i][j] = (double *)malloc(dimensions * sizeof(double));
-            if (new_clusters[i][j] == NULL) {
-                fprintf(stderr, "Memory allocation failed\n");
-                return 1;
-            }
-        }
-    }
-
-
-    for (i = 0; i < num_centroids; i++) {
-        for (j = 0; j < /* Size of cluster */; j++) {
-
-            int closest_centroid_idx;
-            double closest_centroid_distance = INFINITY;
-
-            for (int centroid_idx = 0; centroid_idx < num_centroids; centroid_idx++) {
-                double distance = distance_squared(clusters[i][j], centroids[centroid_idx], dimensions);
-
-                if (distance < closest_centroid_distance) {
-                    closest_centroid_idx = centroid_idx;
-                    closest_centroid_distance = distance;
+bool assign_vectors_to_centroids(Cluster clusters[], int centroids[][MAX_DIMENSION], int dimension, int K, DistanceType distance_type) {
+    bool unchanged = true;
+    for (int k = 0; k < K; k++) {
+        for (int i = 0; i < clusters[k].size; i++) {
+            int closest_centroid_idx = -1;
+            int closest_distance = INT_MAX;
+            for (int j = 0; j < K; j++) {
+                int distance;
+                if (distance_type == MANHATTAN) {
+                    distance = manhattan_distance_squared(clusters[k].vectors[i], clusters[j].vectors[0], dimension);
+                } else {
+                    distance = euclidean_distance_squared(clusters[k].vectors[i], clusters[j].vectors[0], dimension);
+                }
+                if (distance < closest_distance) {
+                    closest_distance = distance;
+                    closest_centroid_idx = j;
                 }
             }
-
-
-            if (LOG) {
-                fprintf(stderr, "\t\t%s closest to %s (before %s)\n", clusters[i][j], centroids[closest_centroid_idx], centroids[i]);
+            if (closest_centroid_idx != k) {
+                unchanged = false;
             }
-
-            memcpy(new_clusters[closest_centroid_idx][j], clusters[i][j], dimensions * sizeof(double));
-
-
-            unchanged = unchanged && closest_centroid_idx == i;
+            clusters[closest_centroid_idx].vectors[clusters[closest_centroid_idx].size++] = clusters[k].vectors[i];
         }
     }
-
-
-    for (i = 0; i < num_centroids; i++) {
-        for (j = 0; j < /* Size of cluster */; j++) {
-            free(clusters[i][j]);
-        }
-        free(clusters[i]);
-    }
-    free(clusters);
-
-
-    clusters = new_clusters;
-
-    return !unchanged;
+    return unchanged;
 }
-
-    
