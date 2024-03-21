8b3e56e61abb653ccc18a1c1affa61a5d1fc2a36
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-21 21:51:09 +0100

    combinaison

--- c-skeleton/tests_file/test_up_centro_fichier.c
+++ c-skeleton/tests_file/test_up_centro_fichier.c
@@ -106,7 +106,8 @@ void test_up_centro_f1() {
     CU_ASSERT_EQUAL(result.data[1].coords[0], (int64_t)2);
     CU_ASSERT_EQUAL(result.data[1].coords[1], (int64_t)2);
     fprintf(stderr, "Le deuxième centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n", result.data[1].coords[0], result.data[1].coords[1]);
-
+    //fprintf(stderr, "taille du premier centro : %" PRId64 " \n", result.size);
+//clusters[i].size
     //fprintf(stderr, "Le deuxième centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n", clusters[1].data[6].coords[0], clusters[0].data[0].coords[1]);
     //fprintf(stderr, "Le deuxième centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n", clusters[1].data[0].coords[0], clusters[1].data[0].coords[1]);
     //fprintf(stderr, "Le deuxième centroid a les coordonnées : (%" PRId64 ",%" PRId64 ") \n", clusters[0].data[1].coords[0], clusters[0].data[1].coords[1]);
