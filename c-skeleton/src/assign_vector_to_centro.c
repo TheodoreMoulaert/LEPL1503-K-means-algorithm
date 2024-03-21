396701b5d4b61601dffe9a4b848987f01939390b
Théodore Moulaert <theodore.moulert@student.uclouvain.be>
2024-03-21 21:05:51 +0100

    test assign vectors

--- c-skeleton/src/assign_vector_to_centro.c
+++ c-skeleton/src/assign_vector_to_centro.c
@@ -3,20 +3,19 @@
 #include "../headers/cluster.h"
 #include <limits.h>
 #include <stdbool.h>
-#include <stdint.h>
 
-bool assign_vectors_to_centroids(cluster_t clusters[], cluster_t centroids[], int64_t dimension, uint64_t K, squared_distance_func_t distance_type) {
+bool assign_vectors_to_centroids(cluster_t clusters[], cluster_t centroids[], uint64_t K, squared_distance_func_t distance_type) {
     bool unchanged = true;
     for (uint64_t k = 0; k < K; k++) {
-        for (int64_t i = 0; i < clusters[k].size; i++) {
+        for (uint64_t i = 0; i < clusters[k].size; i++) {
             int64_t closest_centroid_idx = -1;
             int64_t closest_distance = INT_MAX;
             for (uint64_t j = 0; j < K; j++) {
                 int64_t distance;
                 if (distance_type == squared_manhattan_distance) {
-                    distance = squared_manhattan_distance(&clusters[k].data[i], &centroids[j].center);
+                    distance = squared_manhattan_distance(&clusters[k].data[i], &centroids[j].data[i]);
                 } else {
-                    distance = squared_euclidean_distance(&clusters[k].data[i], &centroids[j].center);
+                    distance = squared_euclidean_distance(&clusters[k].data[i], &centroids[j].data[i]);
                 }
                 if (distance < closest_distance) {
                     closest_distance = distance;
@@ -26,10 +25,8 @@ bool assign_vectors_to_centroids(cluster_t clusters[], cluster_t centroids[], in
             if (closest_centroid_idx != k) {
                 unchanged = false;
             }
-            // Assign the vector to the closest centroid
             centroids[closest_centroid_idx].data[centroids[closest_centroid_idx].size++] = clusters[k].data[i];
         }
     }
     return unchanged;
 }
-
