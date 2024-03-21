6d02ea95188031236f90e731ee8aada672c1205c
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-03-21 22:52:59 +0100

    commentaire

--- c-skeleton/headers/k_means.h
+++ c-skeleton/headers/k_means.h
@@ -8,13 +8,13 @@
 #include "../headers/assign_vector_to_centro.h"
 
 // Fonction pour assigner les vecteurs aux centroids les plus proches
-bool assign_vectors_to_centroids(point_t *centroids, point_t **clusters, uint64_t  num_centroids, uint64_t  num_vectors, uint32_t dimensions);
+//bool assign_vectors_to_centroids(point_t *centroids, point_t **clusters, uint64_t  num_centroids, uint64_t  num_vectors, uint32_t dimensions);
 
 // Fonction pour mettre à jour les centroids
-point_t* update_centroids(point_t **clusters, uint32_t K);
+//point_t* update_centroids(point_t **clusters, uint32_t K);
 
 // Fonction k_means
 //point_t* k_means(point_t *initial_centroids, int K, double **vectors, int num_vectors, int dimensions);
-cluster_t k_means(point_t *initial_centroids, uint32_t K, point_t **vectors, uint64_t num_vectors, uint32_t dimensions);
+cluster_t* k_means(point_t *initial_centroids, uint32_t K, point_t **vectors, uint64_t num_vectors, uint32_t dimensions);
 
 #endif  // K_MEANS_H
