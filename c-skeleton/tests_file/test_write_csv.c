6cd2f00acfb92b8d45eb01b637e333c34a59e28e
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-03-21 21:30:12 +0100

    modif

--- c-skeleton/tests_file/test_write_csv.c
+++ c-skeleton/tests_file/test_write_csv.c
@@ -7,20 +7,25 @@ void test_create_CSV() {
     uint32_t k = 2; // Remplacez 2 par la valeur appropriée selon votre cas
 
     // Création de quelques points fictifs
-    point_t inits[] = {
+    point_t inits1[] = {
         {.dim = 2, .coords = (int64_t[]){1, 2}},
         {.dim = 2, .coords = (int64_t[]){3, 4}},
         {.dim = 2, .coords = (int64_t[]){5, 6}}
     };
+    point_t inits2[] = {
+        {.dim = 2, .coords = (int64_t[]){0, 0}},
+        {.dim = 2, .coords = (int64_t[]){4, 4}},
+        {.dim = 2, .coords = (int64_t[]){4, 6}}
+    };
 
     // Création de quelques clusters fictifs
     cluster_t clusters[] = {
-        {.center = {.dim = 2, .coords = (int64_t[]){2, 2}}, .data = inits, .size = 3},
-        {.center = {.dim = 2, .coords = (int64_t[]){4, 4}}, .data = inits, .size = 3}
+        {.center = {.dim = 2, .coords = (int64_t[]){2, 2}}, .data = inits1, .size = 3},
+        {.center = {.dim = 2, .coords = (int64_t[]){4, 4}}, .data = inits2, .size = 3}
     };
 
     // Appel de la fonction create_CSV
-    int8_t result = create_CSV(inits, clusters, 100, NULL, k);
+    int8_t result = create_CSV(clusters, 100, NULL, k);
 
     CU_ASSERT_EQUAL(result, 0);
 }
