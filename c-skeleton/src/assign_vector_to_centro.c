635678a0ed1a007e42d61f9cd4ff15dcbb6992ba
Victor Defrenne <victor.defrenne@student.uclouvain.be>
2024-03-21 15:34:01 +0000

    Update assign_vector_to_centro.c j'ai mis lew cluster en sluster et les centro en cluster

--- c-skeleton/src/assign_vector_to_centro.c
+++ c-skeleton/src/assign_vector_to_centro.c
@@ -6,7 +6,7 @@
 #include "cluster.h"
 #include <stdint.h> // Include for int64_t and uint64_t
 
-bool assign_vectors_to_centroids(cluster_t clusters[], int64_t centroids[][MAX_DIMENSION], int64_t dimension, uint64_t K, squared_distance_func_t distance_type) {
+bool assign_vectors_to_centroids(cluster_t clusters[], cluster_t centroids[], int64_t dimension, uint64_t K, squared_distance_func_t distance_type) {
     bool unchanged = true;
     for (uint64_t k = 0; k < K; k++) {
         for (int64_t i = 0; i < clusters[k].size; i++) {
@@ -15,9 +15,9 @@ bool assign_vectors_to_centroids(cluster_t clusters[], int64_t centroids[][MAX_D
             for (uint64_t j = 0; j < K; j++) {
                 int64_t distance;
                 if (distance_type == squared_manhattan_distance) {
-                    distance = squared_manhattan_distance(clusters[k].data[i], centroids[j], dimension);
+                    distance = squared_manhattan_distance(clusters[k].data[i], centroids[j].data, dimension);
                 } else {
-                    distance = squared_manhattan_distance(clusters[k].data[i], centroids[j], dimension);
+                    distance = squared_manhattan_distance(clusters[k].data[i], centroids[j].data, dimension);
                 }
                 if (distance < closest_distance) {
                     closest_distance = distance;
@@ -28,7 +28,7 @@ bool assign_vectors_to_centroids(cluster_t clusters[], int64_t centroids[][MAX_D
                 unchanged = false;
             }
             // Assign the vector to the closest centroid
-            clusters[closest_centroid_idx].data[clusters[closest_centroid_idx].size++] = clusters[k].data[i];
+            centroids[closest_centroid_idx].data[centroids[closest_centroid_idx].size++] = clusters[k].data[i];
         }
     }
     return unchanged;
