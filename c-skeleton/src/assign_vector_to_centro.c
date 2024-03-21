742008f5881a9a6849ba5600a321c2a27c15ae69
Théodore Moulaert <theodore.moulert@student.uclouvain.be>
2024-03-21 19:41:37 +0100

    test

--- c-skeleton/src/assign_vector_to_centro.c
+++ c-skeleton/src/assign_vector_to_centro.c
@@ -6,18 +6,18 @@
 #include "cluster.h"
 #include <stdint.h> // Include for int64_t and uint64_t
 
-bool assign_vectors_to_centroids(cluster_t clusters[], cluster_t centroids[], int64_t dimension, uint64_t K, squared_distance_func_t distance_type) {
+bool assign_vectors_to_centroids(cluster_t clusters[], cluster_t centroids[], uint64_t K, squared_distance_func_t distance_type) {
     bool unchanged = true;
     for (uint64_t k = 0; k < K; k++) {
-        for (int64_t i = 0; i < clusters[k].size; i++) {
+        for (uint64_t i = 0; i < clusters[k].size; i++) {
             int64_t closest_centroid_idx = -1;
             int64_t closest_distance = INT_MAX;
             for (uint64_t j = 0; j < K; j++) {
                 int64_t distance;
                 if (distance_type == squared_manhattan_distance) {
-                    distance = squared_manhattan_distance(clusters[k].data[i], centroids[j].data, dimension);
+                    distance = squared_manhattan_distance(&clusters[k].data[i], &centroids[j].data, clusters[k].center.dim);
                 } else {
-                    distance = squared_manhattan_distance(clusters[k].data[i], centroids[j].data, dimension);
+                    distance = squared_euclidean_distance(&clusters[k].data[i], &centroids[j].data, clusters[k].center.dim);
                 }
                 if (distance < closest_distance) {
                     closest_distance = distance;
