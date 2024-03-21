0469c60ceac537c3be0457d0448a96efc7327d44
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-03-21 22:10:00 +0100

    write csv

--- c-skeleton/headers/write_csv.h
+++ c-skeleton/headers/write_csv.h
@@ -2,17 +2,17 @@
 #define WRITE_CSV_H
 #include <stdint.h>
 #include <stdio.h>
-#include <inttypes.h> // Ajout de l'en-tête inttypes.h
+#include <inttypes.h> 
 
 #include "../headers/point.h"
 #include "../headers/cluster.h"
 
-int8_t create_CSV( cluster_t *clusters, uint64_t distortion, uint32_t *clusterParPoint, uint32_t k);
+int8_t create_CSV(cluster_t *clusters, cluster_t centroid_init, uint64_t distortion, uint32_t *clusterParPoint, uint32_t k);
 
 void fprintArrayClusters(FILE *fp, cluster_t *cluster); 
 
 void fprintCoordPoint(FILE *fp, point_t pt);
 
-void fprintArrayPoints(FILE *fp, point_t* arrayPts, uint64_t nb);
+void fprintArrayPoints(FILE *fp, int64_t* arrayPts, uint64_t nb); 
 
 #endif 
--- c-skeleton/tests_file/test_write_csv.c
+++ c-skeleton/tests_file/test_write_csv.c
@@ -3,10 +3,9 @@
 #include "../headers/write_csv.h"
 
 void test_create_CSV() {
-    // Déclaration et initialisation de k
-    uint32_t k = 2; // Remplacez 2 par la valeur appropriée selon votre cas
+    // tests avec des données fictives pour s'assurer du fonctionnement 
+    uint32_t k = 2; 
 
-    // Création de quelques points fictifs
     point_t inits1[] = {
         {.dim = 2, .coords = (int64_t[]){1, 2}},
         {.dim = 2, .coords = (int64_t[]){3, 4}},
@@ -18,43 +17,46 @@ void test_create_CSV() {
         {.dim = 2, .coords = (int64_t[]){4, 6}}
     };
 
-    // Création de quelques clusters fictifs
+
     cluster_t clusters[] = {
         {.center = {.dim = 2, .coords = (int64_t[]){2, 2}}, .data = inits1, .size = 3},
         {.center = {.dim = 2, .coords = (int64_t[]){4, 4}}, .data = inits2, .size = 3}
     };
 
-    // Appel de la fonction create_CSV
-    int8_t result = create_CSV(clusters, 100, NULL, k);
+
+
+    cluster_t centroid_init = {
+        .size = 2,
+        .data = (point_t[]){
+            {.dim = 2, .coords = (int64_t[]){1, 2}},
+            {.dim = 2, .coords = (int64_t[]){3, 4}}
+        }
+    };
+
+
+  
+    int8_t result = create_CSV(clusters, centroid_init, 100, NULL, k);
 
     CU_ASSERT_EQUAL(result, 0);
 }
 
-
 int main() {
-    // Initialize the CUnit test registry
+
     if (CUE_SUCCESS != CU_initialize_registry()) {
         return CU_get_error();
     }
-
-    // Add a suite to the registry
     CU_pSuite pSuite = CU_add_suite("Suite", NULL, NULL);
     if (NULL == pSuite) {
         CU_cleanup_registry();
         return CU_get_error();
     }
-
-    // Add the test function to the suite
     if ((NULL == CU_add_test(pSuite, "test create_CSV", test_create_CSV))) {
         CU_cleanup_registry();
         return CU_get_error();
     }
-
-    // Run the tests
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
 
     return CU_get_error();
 }
-
