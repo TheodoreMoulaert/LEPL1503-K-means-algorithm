5edb696c4cb8dbcb6fc1b7f7c3e80c40817ec978
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-03-22 10:08:44 +0100

    64

--- c-skeleton/src/k_means.c
+++ c-skeleton/src/k_means.c
@@ -58,7 +58,7 @@ cluster_t* k_means(cluster_t initial_centroids, uint32_t K, point_t **vectors, u
         }
     }
 
-    uint34_t changed = 1;
+    uint64_t changed = 1;
     while (changed) {
         changed = assign_vector_to_centroids(centroids, clusters, K, distance_func);
         for (uint32_t i = 0; i < K; i++) {
