2884b01de4893b8aee4154e113eb1d6596b00ae4
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-03-22 11:42:50 +0100

    modif avec manque de free demander

--- c-skeleton/tests_file/test_distortion_update.c
+++ c-skeleton/tests_file/test_distortion_update.c
@@ -10,8 +10,7 @@
 #include "../headers/distortion.h"
 #include "../headers/update_centroids.h"
 
-void test_distortion_with_update_centroids();
-
+void test_distortion_with_update_centroids() ; 
 int init_suite(void) {
     return 0;
 }
@@ -68,24 +67,50 @@ void test_distortion_with_update_centroids() {
     clusters[1].data[2].coords[1] = 1;
 
     cluster_t new_centroids = update_centroids(clusters, taille);
+    printf("Nouveaux centroïdes :\n");
+    for (uint32_t i = 0; i < new_centroids.size; i++) {
+        printf("Centroïde %u : (", i);
+        for (uint32_t j = 0; j < new_centroids.data[i].dim; j++) {
+            printf("%" PRId64, new_centroids.data[i].coords[j]);
+            if (j < new_centroids.data[i].dim - 1) {
+                printf(", ");
+            }
+        }
+        printf(")\n");
+    }
 
-    // Libération de la mémoire des nouveaux centroids
-    for (uint32_t i = 0; i < new_centroids.size; ++i) {
+    // Appel de la fonction distortion avec les nouveaux centres des clusters
+    squared_distance_func_t dummy_func = squared_euclidean_distance;
+    uint64_t result = distortion(clusters, taille, dummy_func);
+    printf("Résultat de la fonction distortion : %lu\n", result);
+
+
+    uint64_t expected_result = 7; // Pour chaque cluster, la somme des carrés des différences entre les coordonnées des points et du centre est 0 + 1*1 + 2*2 = 5. Comme il y a deux clusters, la distorsion totale est 2 * 5 = 10.
+
+  
+    CU_ASSERT_EQUAL(result, expected_result);
+
+    /*for (uint32_t i = 0; i < new_centroids.size; ++i) {
         free(new_centroids.data[i].coords);
     }
-    free(new_centroids.data);
+    free(new_centroids.data);*/
 
-    // Libération de la mémoire des clusters
+  
     for (uint32_t i = 0; i < taille; ++i) {
         free(clusters[i].center.coords);
-        // Libération de la mémoire pour les coordonnées des points à l'intérieur des clusters
+        // Free memory for the coordinates of the points
         for (uint64_t j = 0; j < clusters[i].size; ++j) {
             free(clusters[i].data[j].coords);
         }
+        // Free memory for the data of the clusters
         free(clusters[i].data);
     }
+    // Free memory for clusters
     free(clusters);
+        
 }
+
+
 
 int main() {
     // Initialisation des tests
