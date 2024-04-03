2bf4a6b508744925f85657b9456690f355b50024
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-04-03 12:16:25 +0200

    modif

--- c-skeleton/tests_file/test_distortion.c
+++ c-skeleton/tests_file/test_distortion.c
@@ -7,81 +7,87 @@
 #include "../headers/cluster.h"
 #include "../headers/distance.h"
 
+// Initialisation de la suite de tests
 int init_suite(void) {
     return 0;
 }
 
+// Nettoyage de la suite de tests
 int clean_suite(void) {
     return 0;
 }
 
+// Test de la fonction distortion
 void test_distortion() {
-    // initiation
-    uint32_t k = 2; 
+    // Initialisation des données
+    uint32_t k = 2;
 
-    // construction d'un cluster
+    // Construction des clusters
     cluster_t *clusters = malloc(k * sizeof(cluster_t));
     for (uint32_t i = 0; i < k; ++i) {
-        clusters[i].size = 3; // Example size
-        clusters[i].center.dim = 2;
-        clusters[i].center.coords = malloc(2 * sizeof(int64_t));
-        clusters[i].center.coords[0] = i * 10;
-        clusters[i].center.coords[1] = i * 10 + 1;
-        clusters[i].data = malloc(clusters[i].size * sizeof(point_t));
+        clusters[i].size = 3; // Exemple de taille
+        clusters[i].centroide.dim = 2;
+        clusters[i].centroide.coords = malloc(2 * sizeof(int64_t));
+        clusters[i].centroide.coords[0] = i * 10;
+        clusters[i].centroide.coords[1] = i * 10 + 1;
+        clusters[i].data = malloc(clusters[i].size * sizeof(point_t *));
         for (uint64_t j = 0; j < clusters[i].size; ++j) {
-            clusters[i].data[j].dim = 2;
-            clusters[i].data[j].coords = malloc(2 * sizeof(int64_t));
-            clusters[i].data[j].coords[0] = i * 10 + j;
-            clusters[i].data[j].coords[1] = i * 10 + j + 1;
+            clusters[i].data[j] = malloc(sizeof(point_t));
+            clusters[i].data[j]->dim = 2;
+            clusters[i].data[j]->coords = malloc(2 * sizeof(int64_t));
+            clusters[i].data[j]->coords[0] = i * 10 + j;
+            clusters[i].data[j]->coords[1] = i * 10 + j + 1;
         }
     }
 
-    // quelle distance on utilise pour les tests
+    // Choix de la fonction de distance pour les tests
     squared_distance_func_t dummy_func = squared_euclidean_distance;
 
-    // on applique la fonction distortion
+    // Application de la fonction distortion
     uint64_t result = distortion(clusters, k, dummy_func);
-    
-    // la valeur que l'on devrait obtenir
-    uint64_t expected_result = 20; 
 
-    // on test si c'est bien égale
+    // La valeur attendue
+    uint64_t expected_result = 20;
+
+    // Test si les résultats correspondent
     CU_ASSERT_EQUAL(result, expected_result);
 
-    // clean-up
+    // Nettoyage
     for (uint32_t i = 0; i < k; ++i) {
-        free(clusters[i].center.coords);
+        free(clusters[i].centroide.coords);
         for (uint64_t j = 0; j < clusters[i].size; ++j) {
-            free(clusters[i].data[j].coords);
+            free(clusters[i].data[j]->coords);
+            free(clusters[i].data[j]);
         }
         free(clusters[i].data);
     }
     free(clusters);
 }
 
+// Fonction principale
 int main() {
- 
+    // Initialisation du registre CUnit
     if (CUE_SUCCESS != CU_initialize_registry()) {
         return CU_get_error();
     }
 
-   
+    // Ajout d'une suite de tests
     CU_pSuite pSuite = CU_add_suite("Suite", init_suite, clean_suite);
     if (NULL == pSuite) {
         CU_cleanup_registry();
         return CU_get_error();
     }
 
-
-    if ((NULL == CU_add_test(pSuite, "test of distortion function", test_distortion))) {
+    // Ajout du test à la suite
+    if ((NULL == CU_add_test(pSuite, "Test de la fonction distortion", test_distortion))) {
         CU_cleanup_registry();
         return CU_get_error();
     }
 
-  
+    // Configuration de l'affichage des résultats
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
-    
+
     return CU_get_error();
 }
