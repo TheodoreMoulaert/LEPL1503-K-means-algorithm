40e791f4897cc9d977a34cd7a051a18d7fb24dd6
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-04-03 16:40:35 +0200

    modif

--- c-skeleton/headers/assign_vector_to_centro.h
+++ c-skeleton/headers/assign_vector_to_centro.h
@@ -1,10 +1,13 @@
 #ifndef ASSIGN_VECTORS_TO_CENTROIDS_H
 #define ASSIGN_VECTORS_TO_CENTROIDS_H
 
-#include "cluster.h"
-#include "point.h"
-#include "distance.h" 
+#include "../headers/assign_vector_to_centro.h"
+#include <stddef.h>
+#include "../headers/distance.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-uint64_t assign_vector_to_centroids(cluster_t* centroids, cluster_t* clusters, uint32_t K, squared_distance_func_t distance_func);
+cluster_t** assign_vectors_to_centroides(point_t *centroids, cluster_t **clusters, uint64_t nbr_vectors, uint32_t K, squared_distance_func_t distance_func); 
+
 
 #endif /* ASSIGN_VECTORS_TO_CENTROIDS_H */
