1946c4aca6a394b0c63433eea1f1efacb202ef04
Clémentine Moreau <clementine.moreau@student.uclouvain.be>
2024-04-30 18:32:57 +0200

    test_write_thread ok

--- c-skeleton/tests_file/test_write_thread.c
+++ c-skeleton/tests_file/test_write_thread.c
@@ -8,7 +8,7 @@
 // Définir une fonction main de test
 int main() {
     // Créer un fichier de sortie
-    FILE *output_file = fopen("output_write.csv", "w");
+    FILE *output_file = fopen("test_write_thread.csv", "w");
     if (output_file == NULL) {
         perror("Erreur lors de l'ouverture du fichier de sortie");
         return EXIT_FAILURE;
@@ -21,32 +21,29 @@ int main() {
     int64_t nombre_comb = 3;
 
     // Allouer et initialiser des tableaux de points et de clusters pour le test
-    point_t *centroid_init_Array = malloc(k * sizeof(point_t *));
-    point_t *centroid_final_Array = malloc(k * sizeof(point_t *));
-    cluster_t **clustersArray = malloc(nombre_comb * sizeof(cluster_t **));
+    point_t *centroid_init_Array = malloc(k * sizeof(point_t));
+    point_t *centroid_final_Array = malloc(k * sizeof(point_t));
+    cluster_t **clustersArray = malloc(nombre_comb * sizeof(cluster_t *));
 
     for (int i = 0; i < nombre_comb; i++) {
-        //centroid_init_Array[i] = malloc(k * sizeof(point_t));
-        //centroid_final_Array[i] = malloc(k * sizeof(point_t));
-        clustersArray[i] = malloc(k * sizeof(cluster_t *));
-
+        clustersArray[i] = malloc(k * sizeof(cluster_t));
         for (int j = 0; j < k; j++) {
-            centroid_init_Array[j].dim = dimension;
-            centroid_init_Array[j].coords = malloc(dimension * sizeof(int64_t));
-            centroid_final_Array[j].dim = dimension;
-            centroid_final_Array[j].coords = malloc(dimension * sizeof(int64_t));
-
-            for (int d = 0; d < dimension; d++) {
-                centroid_init_Array[j].coords[d] = i + j + d; // Valeurs arbitraires pour le test
-                centroid_final_Array[j].coords[d] = i * j * d; // Valeurs arbitraires pour le test
+            clustersArray[i][j].size =3; // Taille arbitraire pour le test
+            clustersArray[i][j].data = malloc((3) * sizeof(point_t));
+            for (int l = 0; l <3; l++) {
+                clustersArray[i][j].data[l] = centroid_init_Array; // Utilisation des mêmes points pour le test
             }
+        }
+    }
+    for (int j = 0; j < k; j++) {
+        centroid_init_Array[j].dim = dimension;
+        centroid_init_Array[j].coords = malloc(dimension * sizeof(int64_t));
+        centroid_final_Array[j].dim = dimension;
+        centroid_final_Array[j].coords = malloc(dimension * sizeof(int64_t));
 
-            //clustersArray[i][j] = NULL;//malloc(sizeof(cluster_t));
-            clustersArray[i][j].size = j + 1; // Taille arbitraire pour le test
-            clustersArray[i][j].data = malloc((j + 1) * sizeof(point_t *));
-            for (int l = 0; l <= j; l++) {
-                clustersArray[i][j].data[l] = &centroid_init_Array[j]; // Utilisation des mêmes points pour le test
-            }
+        for (int d = 0; d < dimension; d++) {
+            centroid_init_Array[j].coords[d] =  j + d; // Valeurs arbitraires pour le test
+            centroid_final_Array[j].coords[d] =  j * d; // Valeurs arbitraires pour le test
         }
     }
 
@@ -56,24 +53,17 @@ int main() {
     // Libérer la mémoire allouée
     for (int i = 0; i < nombre_comb; i++) {
         for (int j = 0; j < k; j++) {
-            //free(centroid_init_Array[i][j].coords);
-            //free(centroid_final_Array[i][j].coords);
             free(clustersArray[i][j].data);
-            //free(clustersArray[i][j]);
         }
-        //free(centroid_init_Array[i]);
-        //free(centroid_final_Array[i]);
         free(clustersArray[i]);
     }
     for (int j = 0; j < k; j++) {
         free(centroid_init_Array[j].coords);
         free(centroid_final_Array[j].coords);
-       // free(centroid_init_Array[j]);
-       // free(centroid_final_Array[j]);
     }
+    free(clustersArray);
     free(centroid_init_Array);
     free(centroid_final_Array);
-    free(clustersArray);
 
     // Fermer le fichier de sortie
     fclose(output_file);
