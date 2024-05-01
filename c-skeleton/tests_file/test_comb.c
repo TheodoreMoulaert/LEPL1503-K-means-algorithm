65f4582b7abe1ddbff7eb5aafa3f3313a80f16f5
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-05-01 11:37:29 +0200

    ajout assert

--- c-skeleton/tests_file/test_comb.c
+++ c-skeleton/tests_file/test_comb.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <assert.h>
 #include <stdlib.h>
 #include <inttypes.h>
 #include <stdbool.h>
@@ -7,8 +6,17 @@
 #include "../headers/cluster.h"
 #include "../headers/distance.h"
 #include "../headers/combinaison.h"
+#include "CUnit/Basic.h"
 
-int main() {
+int init_suite(void) {
+    return 0;
+}
+
+int clean_suite(void) {
+    return 0;
+}
+
+void test_combinations(void) {
     int64_t coordinates[7][2] = {
         {1, 1},
         {2, 2},
@@ -20,22 +28,15 @@ int main() {
     };
 
     point_t **vectors = malloc(7 * sizeof(point_t *));
-    if (vectors == NULL) {
-        fprintf(stderr, "Erreur d'allocation mémoire pour les points.\n");
-        return EXIT_FAILURE;
-    }
+    CU_ASSERT_PTR_NOT_NULL_FATAL(vectors);
 
     for (int i = 0; i < 7; i++) {
         vectors[i] = malloc(sizeof(point_t));
-        if (vectors[i] == NULL) {
-            fprintf(stderr, "Erreur d'allocation mémoire pour un point.\n");
-            return EXIT_FAILURE;
-        }
+        CU_ASSERT_PTR_NOT_NULL_FATAL(vectors[i]);
+        
         vectors[i]->coords = malloc(2 * sizeof(int64_t));
-        if (vectors[i]->coords == NULL) {
-            fprintf(stderr, "Erreur d'allocation mémoire pour les coordonnées d'un point.\n");
-            return EXIT_FAILURE;
-        }
+        CU_ASSERT_PTR_NOT_NULL_FATAL(vectors[i]->coords);
+        
         vectors[i]->coords[0] = coordinates[i][0];
         vectors[i]->coords[1] = coordinates[i][1];
         vectors[i]->dim = 2;
@@ -46,21 +47,25 @@ int main() {
     uint32_t p = 3;
 
     point_t ***combinations = generate_combinations(vectors, n, k, p);
-    if (combinations == NULL) {
-        fprintf(stderr, "Erreur lors de la génération des combinaisons.\n");
-        return EXIT_FAILURE;
-    }
+    CU_ASSERT_PTR_NOT_NULL_FATAL(combinations);
 
-    printf("Combinaisons générées :\n");
-    for (uint64_t i = 0; i < combinaison(p, k); i++) {
-        printf("Combinaison %lu :\n", i + 1);
-        for (uint32_t j = 0; j < k; j++) {
-            printf("(%" PRId64 ", %" PRId64 ")\n", combinations[i][j]->coords[0], combinations[i][j]->coords[1]);
-        }
-        printf("\n");
-    }
+    // Test combinations
+    CU_ASSERT_EQUAL(combinations[0][0]->coords[0], 1);
+    CU_ASSERT_EQUAL(combinations[0][0]->coords[1], 1);
+    CU_ASSERT_EQUAL(combinations[0][1]->coords[0], 2);
+    CU_ASSERT_EQUAL(combinations[0][1]->coords[1], 2);
 
-    // Libérer la mémoire
+    CU_ASSERT_EQUAL(combinations[1][0]->coords[0], 1);
+    CU_ASSERT_EQUAL(combinations[1][0]->coords[1], 1);
+    CU_ASSERT_EQUAL(combinations[1][1]->coords[0], 3);
+    CU_ASSERT_EQUAL(combinations[1][1]->coords[1], 4);
+
+    CU_ASSERT_EQUAL(combinations[2][0]->coords[0], 2);
+    CU_ASSERT_EQUAL(combinations[2][0]->coords[1], 2);
+    CU_ASSERT_EQUAL(combinations[2][1]->coords[0], 3);
+    CU_ASSERT_EQUAL(combinations[2][1]->coords[1], 4);
+
+    // Free memory
     for (uint64_t i = 0; i < combinaison(p, k); i++) {
         free(combinations[i]);
     }
@@ -71,6 +76,28 @@ int main() {
         free(vectors[i]);
     }
     free(vectors);
+}
 
-    return EXIT_SUCCESS;
+int main() {
+    CU_pSuite pSuite = NULL;
+
+    if (CUE_SUCCESS != CU_initialize_registry()) {
+        return CU_get_error();
+    }
+
+    pSuite = CU_add_suite("Suite de tests", init_suite, clean_suite);
+    if (NULL == pSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if ((NULL == CU_add_test(pSuite, "test_combinations", test_combinations))) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
 }
