0f62d271a489a40daed2ee950eec493131803ccd
Pauline Meerman <pauline.meerman@student.uclouvain.be>
2024-05-03 14:06:05 +0200

    com

--- c-skeleton/headers/k_means.h
+++ c-skeleton/headers/k_means.h
@@ -7,6 +7,17 @@
 #include "../headers/update_centroids.h"
 #include "../headers/assign_vector_to_centro.h"
 
+/**
+ * @brief Applique l'algorithme de K-means pour regrouper les points en k clusters.
+ * 
+ * @param clusters Tableau de pointeurs vers les clusters initiaux
+ * @param num_points Nombre total de points à regrouper
+ * @param k Nombre de clusters à former
+ * @param initial_centroids Tableau des centroids initiaux
+ * @param final_centroids Tableau des centroids finaux mis à jour par l'algorithme
+ * @param distance_func Fonction de calcul de distance entre les points
+ * @return Tableau de pointeurs vers les clusters finaux, ou NULL en cas d'erreur
+ */
 cluster_t** k_means(cluster_t**clusters_input, uint64_t num_points, uint32_t k, point_t *initial_centroids, point_t *final_centroids, squared_distance_func_t distance_func) ; 
 
 
--- c-skeleton/headers/write_csv.h
+++ c-skeleton/headers/write_csv.h
@@ -7,8 +7,37 @@
 #include <inttypes.h> 
 #include "../headers/point.h"
 #include "../headers/cluster.h"
+/**
+ * Écrit les centroïdes dans un fichier au format spécifié.
+ * 
+ * @param file Pointeur vers le fichier dans lequel écrire les centroïdes.
+ * @param centroid Tableau contenant les centroïdes des clusters.
+ * @param k Nombre total de centroïdes à écrire.
+ * @param dimension Dimension des points dans les centroïdes.
+ */
+void write_centroid(FILE *file, point_t* centroid, int64_t k, int64_t dimension);
 
-void write_centroid(FILE *file, point_t* centroid, int64_t k, int64_t dimension); 
+/**
+ * Écrit les clusters dans un fichier au format spécifié.
+ * 
+ * @param file Pointeur vers le fichier dans lequel écrire les clusters.
+ * @param cluster Tableau de pointeurs vers les clusters.
+ * @param k Nombre total de clusters.
+ * @param dimension Dimension des points dans les clusters.
+ */
 void write_cluster(FILE *file, cluster_t **cluster, int64_t k, int64_t dimension); 
+
+/**
+ * @brief Écrit les données de clustering dans un fichier CSV.
+ * 
+ * @param output_file Pointeur vers le fichier de sortie
+ * @param distortion Tableau des valeurs de distorsion
+ * @param centroid_init_Array Tableau des centroids initiaux pour chaque combinaison
+ * @param centroid_final_Array Tableau des centroids finaux pour chaque combinaison
+ * @param clustersArray Tableau de tableaux de pointeurs vers les clusters pour chaque combinaison
+ * @param k Nombre de clusters
+ * @param dimension Nombre de dimensions des données
+ * @param nombre_comb Nombre total de combinaisons
+ */
 void write_csv(FILE *output_file, uint64_t* distortion, point_t **centroid_init_Array, point_t **centroid_final_Array, cluster_t ***clustersArray, int64_t k, int64_t dimension, int64_t nombre_comb, bool quiet_mode ); 
 #endif //WRITE_CSV_H
