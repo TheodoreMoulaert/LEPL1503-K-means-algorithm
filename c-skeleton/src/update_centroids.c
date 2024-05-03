d6a1140357fd8799ba24ad630fdcf33c651ab4b3
Camille Lefevre <camille.lefevre@student.uclouvain.be>
2024-05-03 21:18:50 +0200

    tests

--- c-skeleton/src/update_centroids.c
+++ c-skeleton/src/update_centroids.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-
+#include <inttypes.h>
 #include "../headers/point.h"
 #include "../headers/cluster.h"
 #include "../headers/update_centroids.h"
@@ -38,10 +38,13 @@ cluster_t** update_centroids(cluster_t *clusters[], uint32_t K) {
             }
         }
     
-        printf("update moyenne[0] = %ld\n", moyenne[0]);
+        printf("update moyenne[0] = %" PRId64 "\n", moyenne[0]);
 
         for (uint32_t xi = 0; xi < dim; xi++) {
-            moyenne[xi] /= clusters[i]->size; // Calcul de la moyenne
+            int64_t taille = (int64_t) clusters[i]->size; 
+            moyenne[xi] = moyenne[xi] / taille; // Calcul de la moyenne
+            printf("Taille du cluster %d après mise à jour des centroïdes : %" PRIu64 "\n", i, clusters[i]->size);
+            printf("update moyenne[xi] = %" PRId64 "\n", moyenne[xi]);
             clusters[i]->centroide.coords[xi] = moyenne[xi]; // Mise à jour du centroïde
             clusters[i]->centroide.dim = dim; 
         }
