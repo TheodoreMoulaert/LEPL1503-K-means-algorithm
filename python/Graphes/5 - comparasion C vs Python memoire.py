43d586f91d5a19227111175aa8f8f5b400c8d6be
Victor Defrenne <victor.defrenne@student.uclouvain.be>
2024-05-04 15:48:49 +0000

    Update 5 - comparasion C vs Python memoire.py

--- python/Graphes/5 - comparasion C vs Python memoire.py
+++ python/Graphes/5 - comparasion C vs Python memoire.py
@@ -1,23 +1,21 @@
 import matplotlib.pyplot as plt
 
 # Tailles de fichiers
-tailles_fichier = ["7 points", "50 points", "100 points"]
+tailles_fichier = ["2 threads", "4 threads", "6 threads","8 threads","10 threads"]
 
 # Données de consommation de mémoire pour Python (exemples)
-memoire_python = [100, 200, 300]  # Exemple de liste de consommation de mémoire pour Python
-memoire_C = [50, 150, 250]  # Exemple de liste de consommation de mémoire pour C
+memoire_C = [39966, 40510, 41054, 41598,42142 ]  # Exemple de liste de consommation de mémoire pour C
 
 # Création du graphique
 plt.figure(figsize=(10, 6))
 
 # Tracé des lignes pour la consommation de mémoire de Python et de C
-plt.plot(tailles_fichier, memoire_python, label='Python', marker='o')
 plt.plot(tailles_fichier, memoire_C, label='C', marker='o')
 
 # Configuration du graphique
-plt.title("Comparaison de la consommation de mémoire entre C et Python")
+plt.title("Comparaison de la consommation en fonction du nombre de threads")
 plt.xlabel("Taille du fichier")
-plt.ylabel("Consommation de mémoire [Mo]")
+plt.ylabel("Consommation de mémoire [bytes]")
 plt.grid(True)
 plt.legend()
 
